24ca765acb0ed4ab90005aedb671bf7dc71f9719
annlng <annlng-8@student.ltu.se>
2019-05-20 12:44:24 +0200

    börjat bygga lånsidan

--- src/GUI/LoanGUI.java
+++ src/GUI/LoanGUI.java
@@ -5,19 +5,30 @@
  */
 package GUI;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Rebec
  */
 public class LoanGUI extends javax.swing.JFrame {
 
+    private final DefaultListModel model;
+    
     /**
      * Creates new form Loan
      */
     public LoanGUI() {
+        super("Loan item");
+        model = new DefaultListModel();
         initComponents();
+        initList();
     }
 
+    private void initList(){
+        lstLoan.setModel(model);
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -35,9 +46,11 @@ public class LoanGUI extends javax.swing.JFrame {
         btnLoanToSearch = new javax.swing.JButton();
         labelLoan = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        listLoan = new javax.swing.JList<>();
+        lstLoan = new javax.swing.JList<>();
         btnLoanLoan = new javax.swing.JButton();
         btnLoanSignOut = new javax.swing.JButton();
+        btnAdd = new javax.swing.JButton();
+        btnRemove = new javax.swing.JButton();
 
         labelLoanConfirm.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         labelLoanConfirm.setText("<html>\n<body>\n<p>\n<center>The loan was successful!</center>\n<br><center>You can see an overview of your borrowed items under \"My Page\".</center>\n</p>\n</body>");
@@ -75,18 +88,47 @@ public class LoanGUI extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         btnLoanHomePage.setText("Home Page");
+        btnLoanHomePage.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLoanHomePageActionPerformed(evt);
+            }
+        });
 
         btnLoanToSearch.setText("To Search");
+        btnLoanToSearch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLoanToSearchActionPerformed(evt);
+            }
+        });
 
         labelLoan.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
         labelLoan.setText("Loan");
         labelLoan.setFocusable(false);
 
-        jScrollPane1.setViewportView(listLoan);
+        jScrollPane1.setViewportView(lstLoan);
 
         btnLoanLoan.setText("Loan");
 
         btnLoanSignOut.setText("Sign out");
+        btnLoanSignOut.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLoanSignOutActionPerformed(evt);
+            }
+        });
+
+        btnAdd.setText("Add item");
+        btnAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddActionPerformed(evt);
+            }
+        });
+
+        btnRemove.setText("Remove item");
+        btnRemove.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRemoveActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -103,12 +145,17 @@ public class LoanGUI extends javax.swing.JFrame {
                                     .addComponent(btnLoanToSearch, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(77, 77, 77)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(btnLoanLoan)
-                                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 511, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 511, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(btnRemove, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                         .addGap(0, 76, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(btnLoanLoan)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(btnLoanSignOut)))
                 .addContainerGap())
             .addGroup(layout.createSequentialGroup()
@@ -127,15 +174,52 @@ public class LoanGUI extends javax.swing.JFrame {
                 .addComponent(labelLoan, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 166, Short.MAX_VALUE)
-                .addGap(18, 18, 18)
-                .addComponent(btnLoanLoan)
-                .addGap(12, 12, 12)
-                .addComponent(btnLoanSignOut)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnAdd)
+                    .addComponent(btnRemove))
+                .addGap(24, 24, 24)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnLoanSignOut)
+                    .addComponent(btnLoanLoan))
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnLoanHomePageActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoanHomePageActionPerformed
+        // TODO add your handling code here:
+        HomePageGui gui = new HomePageGui();
+        gui.setVisible(true);
+    }//GEN-LAST:event_btnLoanHomePageActionPerformed
+
+    private void btnLoanToSearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoanToSearchActionPerformed
+        // TODO add your handling code here:
+        SearchGUI gui = new SearchGUI();
+        gui.setVisible(true);
+    }//GEN-LAST:event_btnLoanToSearchActionPerformed
+
+    private void btnLoanSignOutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoanSignOutActionPerformed
+        // TODO add your handling code here:
+        HomePageGui gui = new HomePageGui();
+        gui.setVisible(true);
+    }//GEN-LAST:event_btnLoanSignOutActionPerformed
+
+    private void btnRemoveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemoveActionPerformed
+        int index = lstLoan.getSelectedIndex();
+//        things.remove(index);
+        model.remove(index);
+        
+    }//GEN-LAST:event_btnRemoveActionPerformed
+
+    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+        String input = JOptionPane.showInputDialog(rootPane, "Which barcode do you wanna add to your loan?");
+        
+//        model.addElement(txtAdd.getText());
+//        things.add(txtAdd.getText());
+//        txtAdd.setText("");
+    }//GEN-LAST:event_btnAddActionPerformed
 
     /**
      * @param args the command line arguments
@@ -174,16 +258,18 @@ public class LoanGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAdd;
     private javax.swing.JButton btnLoanConfMyPage;
     private javax.swing.JButton btnLoanConfSearch;
     private javax.swing.JButton btnLoanHomePage;
     private javax.swing.JButton btnLoanLoan;
     private javax.swing.JButton btnLoanSignOut;
     private javax.swing.JButton btnLoanToSearch;
+    private javax.swing.JButton btnRemove;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel labelLoan;
     private javax.swing.JLabel labelLoanConfirm;
-    private javax.swing.JList<String> listLoan;
+    private javax.swing.JList<String> lstLoan;
     private javax.swing.JPanel panelLoanConfirm;
     // End of variables declaration//GEN-END:variables
 }
--- src/controlClasses/LoanControl.java
+++ src/controlClasses/LoanControl.java
@@ -5,18 +5,46 @@
  */
 package controlClasses;
 
+import JDBCconnection.JDBCconnection;
+import item.Copy;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author annalangstrom
  */
 public class LoanControl {
     
-    public void logInBorrower(String email, String password){
-        
+    private final String COPY_SELECT = "SELECT * FROM Copy WHERE barcodeNo = ?";
+    
+    private final PreparedStatement selectCopy;
+    
+     JDBCconnection connection = new JDBCconnection();
+    private Connection con = null;
+
+    //Konstruktor
+    public LoanControl() throws ClassNotFoundException, 
+            SQLException{
+       //Koppla upp
+       
+       con = connection.connectToDb(con); 
+       selectCopy = con.prepareStatement(COPY_SELECT);
     }
     
     public void createLoanItem(int barcode){}
     
-    public void printReceipt(){}
+    public void printReceipt(ArrayList<Copy> copies){}
+    
+    public void getCopyFromDB(int barcode) throws SQLException{
+        selectCopy.setInt(1, barcode);
+        ResultSet rs = selectCopy.executeQuery();
+        while(rs.next()){
+            
+        }
+    }
     
 }
