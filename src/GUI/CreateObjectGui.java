7a6d7fccbed4d651ff005bbafd20d5e1dcdfb988
Tsifteteli <carbol-8@student.ltu.se>
2019-05-22 17:01:04 +0200

    Ändrat default close operation

--- src/GUI/CreateObjectGui.java
+++ src/GUI/CreateObjectGui.java
@@ -108,7 +108,7 @@ public class CreateObjectGui extends javax.swing.JFrame {
 
       jLabel8.setText("Location*");
 
-      setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+      setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
       jLabel1.setFont(new java.awt.Font("Lucida Grande", 0, 24)); // NOI18N
       jLabel1.setText("New item");
