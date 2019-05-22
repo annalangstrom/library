1699dbbaa0b9043201091e7952bafd08fd55107b
annlng <annlng-8@student.ltu.se>
2019-05-22 22:49:53 +0200

    bytt namn på klassen

--- src/controlClasses/ObjectControl.java
+++ src/controlClasses/ObjectControl.java
@@ -20,7 +20,7 @@ import javax.swing.JOptionPane;
  *
  * @author annalangstrom
  */
-public class CreateObjectControl {
+public class ObjectControl {
     
     private final String BOOK_INSERT = "insert into Item (isbn, title, publisher, "
             + "publishYear, location) values (?, ?, ?, ?, ?)";
@@ -32,9 +32,21 @@ public class CreateObjectControl {
     private final String GENRE_INSERT = "insert into Genre (itemNo, genre) values (?, ?)";
     private final String AUTHOR_ARTIST_INSERT = "insert into AuthorArtist (fName, sName) values (?, ?)";
     private final String AA_ITEM_INSERT = "insert into AAitem (itemNo, aNo) values (?, ?)";
-    private final String AUTHOR_ARTIST_SELECT = "select * from AuthorArtist";
+    private final String AUTHOR_ARTIST_SELECTALL = "select * from AuthorArtist";
+    private final String ITEM_SELECT = "SELECT * FROM Item WHERE itemNo = ?";
+    private final String KEYWORD_SELECT = "SELECT * FROM Keyword WHERE itemNo = ?";
+    private final String GENRE_SELECT = "SELECT * FROM Genre WHERE itemNo = ?";
+    private final String AA_ITEM_SELECT = "SELECT * FROM AAitem WHERE itemNo = ?";
+    private final String AUTART_SELECT = "SELECT * FROM AuthorArtist WHERE aNo = ?";
+    private final String ITEM_UPDATE = "UPDATE Item SET isbn = ?, title = ?, ageLimit = ?, "
+            + "pCountry = ?, publisher = ?, publishYear = ?, location = ? WHERE itemNo = ?";
     
-    
+    private final PreparedStatement selectItem;
+    private final PreparedStatement selectKeyword;
+    private final PreparedStatement selectGenre;
+    private final PreparedStatement selectAAitem;
+    private final PreparedStatement selectAllAutArt;
+    private final PreparedStatement updateItem;
     private final PreparedStatement insertBook;
     private final PreparedStatement insertMagazine;
     private final PreparedStatement insertMovie;
@@ -48,7 +60,7 @@ public class CreateObjectControl {
     private Connection con = null;
 
     //Konstruktor
-    public CreateObjectControl() throws ClassNotFoundException, 
+    public ObjectControl() throws ClassNotFoundException, 
             SQLException{
        //Koppla upp
        
@@ -60,7 +72,13 @@ public class CreateObjectControl {
        insertGenre = con.prepareStatement(GENRE_INSERT);
        insertAutArt = con.prepareStatement(AUTHOR_ARTIST_INSERT, Statement.RETURN_GENERATED_KEYS);
        insertAAitem = con.prepareStatement(AA_ITEM_INSERT);
-       selectAutArt = con.prepareStatement(AUTHOR_ARTIST_SELECT);
+       selectAllAutArt = con.prepareStatement(AUTHOR_ARTIST_SELECTALL);
+       selectItem = con.prepareStatement(ITEM_SELECT);
+       selectKeyword = con.prepareStatement(KEYWORD_SELECT);
+       selectGenre = con.prepareStatement(GENRE_SELECT);
+       selectAAitem = con.prepareStatement(AA_ITEM_SELECT);
+       selectAutArt = con.prepareStatement(AUTART_SELECT);
+       updateItem = con.prepareStatement(ITEM_UPDATE);
     }
     
     public void confirmSaving(){
@@ -267,5 +285,96 @@ public class CreateObjectControl {
     public ResultSet getAAfromDB() throws SQLException{
         ResultSet rs = selectAutArt.executeQuery();
         return rs;
+    }
+    
+    public void updateInfo(Item item) throws SQLException{
+    updateItem.setString(1, item.getTitle());
+    }
+    
+    public Item getItemFromDB(int itemNo) throws SQLException, ClassNotFoundException{
+        ResultSet rs;
+        String isbn = null;
+        String publisher = null;
+        String title = null;
+        int publishYear = 0;
+        String location = null;
+        int ageLimit = 0;
+        String pCountry = null;
+        ArrayList<String> keywords = new ArrayList<>();
+        ArrayList<String> genres = new ArrayList<>();
+        ArrayList<AuthorArtist> authorArtists = new ArrayList<>();
+        Book book;
+        Movie movie;
+        Magazine magazine;
+        
+        selectItem.setInt(1, itemNo);
+        rs = selectItem.executeQuery();
+        while(rs.next()){
+            isbn = rs.getString("isbn");
+            publisher = rs.getString("publisher");
+            title = rs.getString("title");
+            publishYear = rs.getInt("publishYear");
+            location = rs.getString("location");
+            ageLimit = rs.getInt("ageLimit");
+            pCountry = rs.getString("pCountry");
+        }
+        
+        selectKeyword.setInt(1, itemNo);
+        ResultSet rs2 = selectKeyword.executeQuery();
+        while(rs2.next()){
+            String keyword = rs2.getString("word");
+            keywords.add(keyword);
+        }
+        
+        selectGenre.setInt(1, itemNo);
+        rs = selectGenre.executeQuery();
+        while(rs.next()){
+            String genre = rs.getString("genre");
+            genres.add(genre);
+        }
+        
+        selectAAitem.setInt(1, itemNo);
+        rs = selectAAitem.executeQuery();
+        while(rs.next()){
+            int aNo = rs.getInt("aNo");
+            selectAutArt.setInt(1, aNo);
+            ResultSet rs3 = selectAutArt.executeQuery();
+            while(rs3.next()){
+                String fname = rs3.getString("fName");
+                String sname = rs3.getString("sName");
+                AuthorArtist autArt = new AuthorArtist(fname, sname, itemNo);
+                authorArtists.add(autArt);
+            }
+        }
+        
+        if(ageLimit == 0 && pCountry == null){
+            book = new Book(isbn, publisher, title, publishYear, location, 
+                keywords, genres, authorArtists);
+            return book;
+        }
+        else if(isbn == null && publisher == null){
+            movie = new Movie(ageLimit, pCountry, title, publishYear, location, 
+                    keywords, genres, authorArtists);
+            return movie;
+        }
+        else if(isbn == null && ageLimit == 0 && pCountry == null){
+            magazine = new Magazine(publisher, title, publishYear, location, 
+                    keywords, genres, authorArtists);
+            return magazine;
+        }
+        else
+            return null;
+    }
+    
+    public void updateObject(String isbn, String title, int ageLimit, String pCountry,
+            String publisher, int publishYear, String location) throws SQLException{
+        updateItem.setString(1, isbn);
+        updateItem.setString(2, title);
+        updateItem.setInt(3, ageLimit);
+        updateItem.setString(4, pCountry);
+        updateItem.setString(5, publisher);
+        updateItem.setInt(6, publishYear);
+        updateItem.setString(7, location);
+        updateItem.executeUpdate();
     }
 }
