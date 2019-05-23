ca3d7ce1cb1a8a73c3cb30dc32e25a4957fe4bd7
Tsifteteli <carbol-8@student.ltu.se>
2019-05-23 10:48:26 +0200

    Skapat ny klass

---
+++ src/controlClasses/SignInStaffControl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlClasses;
+
+import GUI.HomePageGui;
+import persons.*;
+import GUI.SignInGui;
+import JDBCconnection.JDBCconnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class SignInStaffControl {
+   
+   
+   
+}
