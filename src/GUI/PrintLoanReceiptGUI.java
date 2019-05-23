3f7cd59faa0d19ca0246f45dbc7e53a4fd28b894
annlng <annlng-8@student.ltu.se>
2019-05-23 21:22:13 +0200

    städat

--- src/GUI/PrintLoanReceiptGUI.java
+++ src/GUI/PrintLoanReceiptGUI.java
@@ -72,7 +72,7 @@ public class PrintLoanReceiptGUI extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("Lending date:");
 
