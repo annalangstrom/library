2b5fccc59242799d1098bd3ebaff9c698b244161
Tsifteteli <carbol-8@student.ltu.se>
2019-05-23 14:19:57 +0200

    Lagt till utseende hos returnGui

--- src/GUI/ReturnGui.java
+++ src/GUI/ReturnGui.java
@@ -11,11 +11,13 @@ package GUI;
  */
 public class ReturnGui extends javax.swing.JFrame {
 
-   /**
-    * Creates new form ReturnGui
-    */
-   public ReturnGui() {
+   private HomePageGui homePage;
+   
+   
+   public ReturnGui(HomePageGui homePage) {
       initComponents();
+      this.homePage = homePage;
+      
    }
 
    /**
@@ -27,21 +29,77 @@ public class ReturnGui extends javax.swing.JFrame {
    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
    private void initComponents() {
 
+      labelLoan = new javax.swing.JLabel();
+      btnHomePage = new javax.swing.JButton();
+      txtEnterBarcode = new javax.swing.JTextField();
+      jLabel1 = new javax.swing.JLabel();
+      jLabel2 = new javax.swing.JLabel();
+
       setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+      labelLoan.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+      labelLoan.setText("Return");
+      labelLoan.setFocusable(false);
+
+      btnHomePage.setText("Home Page");
+      btnHomePage.addActionListener(new java.awt.event.ActionListener() {
+         public void actionPerformed(java.awt.event.ActionEvent evt) {
+            btnHomePageActionPerformed(evt);
+         }
+      });
+
+      txtEnterBarcode.setForeground(new java.awt.Color(153, 153, 153));
+      txtEnterBarcode.setText("enter barcode...");
+
+      jLabel2.setText("Please enter the barcode on the item you want to return:");
 
       javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
       getContentPane().setLayout(layout);
       layout.setHorizontalGroup(
          layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-         .addGap(0, 400, Short.MAX_VALUE)
+         .addGroup(layout.createSequentialGroup()
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+               .addGroup(layout.createSequentialGroup()
+                  .addContainerGap()
+                  .addComponent(btnHomePage)
+                  .addGap(105, 105, 105)
+                  .addComponent(labelLoan, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE))
+               .addGroup(layout.createSequentialGroup()
+                  .addGap(123, 123, 123)
+                  .addComponent(jLabel1))
+               .addGroup(layout.createSequentialGroup()
+                  .addGap(102, 102, 102)
+                  .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                     .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                     .addComponent(txtEnterBarcode))))
+            .addContainerGap(109, Short.MAX_VALUE))
       );
       layout.setVerticalGroup(
          layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-         .addGap(0, 300, Short.MAX_VALUE)
+         .addGroup(layout.createSequentialGroup()
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+               .addGroup(layout.createSequentialGroup()
+                  .addGap(22, 22, 22)
+                  .addComponent(labelLoan, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+               .addGroup(layout.createSequentialGroup()
+                  .addContainerGap()
+                  .addComponent(btnHomePage)))
+            .addGap(72, 72, 72)
+            .addComponent(jLabel1)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(jLabel2)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(txtEnterBarcode, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addContainerGap(154, Short.MAX_VALUE))
       );
 
       pack();
    }// </editor-fold>//GEN-END:initComponents
+
+   private void btnHomePageActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHomePageActionPerformed
+      homePage.setVisible(true);
+      super.dispose();
+   }//GEN-LAST:event_btnHomePageActionPerformed
 
 //   /**
 //    * @param args the command line arguments
@@ -79,5 +137,10 @@ public class ReturnGui extends javax.swing.JFrame {
 //   }
 
    // Variables declaration - do not modify//GEN-BEGIN:variables
+   private javax.swing.JButton btnHomePage;
+   private javax.swing.JLabel jLabel1;
+   private javax.swing.JLabel jLabel2;
+   private javax.swing.JLabel labelLoan;
+   private javax.swing.JTextField txtEnterBarcode;
    // End of variables declaration//GEN-END:variables
 }
