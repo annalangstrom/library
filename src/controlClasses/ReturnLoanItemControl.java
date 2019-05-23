05cde6abc1a10c2f069d1c32b1f962a104673385
Tsifteteli <carbol-8@student.ltu.se>
2019-05-23 14:57:39 +0200

    Börjat skriva på return funktionalitet

---
+++ src/controlClasses/ReturnLoanItemControl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlClasses;
+
+/**
+ *
+ * @author annalangstrom
+ */
+public class ReturnLoanItemControl {
+   
+
+   //Ta barcoden på återlämnat item
+   //Skicka in statement i databasen som säger åt den att:
+   // - sätta dagens datum på actualReturnDate
+   // - där barcoden = barcode & actualReturnDate = null
+    public void cancelLoanItem(int barcode){
+        
+    }
+    
+  
+}
